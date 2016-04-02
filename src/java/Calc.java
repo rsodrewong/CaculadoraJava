07ff013b49d7349af14ad6f50b22f1502337830d
rogerio <rogerio@lenovo-g50.wong>
2016-04-02 17:16:35 -0300

    Primeiro repositorio

---
+++ src/java/Calc.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rogerio
+ */
+public class Calc {
+    private int n1, n2;
+
+    /**
+     * @return the n1
+     */
+    public int getN1() {
+        return n1;
+    }
+
+    /**
+     * @param n1 the n1 to set
+     */
+    public void setN1(int n1) {
+        this.setN1(n1);
+    }
+
+    /**
+     * @return the n2
+     */
+    public int getN2() {
+        return n2;
+    }
+
+    /**
+     * @param n2 the n2 to set
+     */
+    public void setN2(int n2) {
+        this.setN2(n2);
+    }
+
+    /**
+     * @param n1 the n1 to set
+     */
+    public void setN1(float n1) {
+        this.n1 = n1;
+    }
+
+    /**
+     * @param n2 the n2 to set
+     */
+    public void setN2(float n2) {
+        this.n2 = n2;
+    }
+}
